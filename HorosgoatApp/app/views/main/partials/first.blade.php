c7fb97a49107cf329b1a0311ad17aba7279a8830
Ksenia Karelskaya <kusksu@gmail.com>
2015-01-11 17:43:08 +0100

    unsignificant css changes (margins/font size/ etc)

--- HorosgoatApp/app/views/main/partials/first.blade.php
+++ HorosgoatApp/app/views/main/partials/first.blade.php
@@ -1,17 +1,19 @@
-<div class="logo">
-	{{HTML::image("img/step.png")}}
-</div>
+<div class="wrap">
+	<div class="logo">
+		{{HTML::image("img/step.png")}}
+	</div>
 
-<div class="text firsttext">
-	<p>New Chinese Year of the <span class="red bold">goat</span> is coming!</p>
-	<p>Want to know what the <span class="blue bold">future</span> holds for you?</p>
-	<p>Get your personal <span class="yellow bold">horoscope</span> in a cookie!</p>
-</div>
+	<div class="text firsttext">
+		<p>New Chinese Year of the <span class="red bold">goat</span> is coming!</p>
+		<p>Want to know what the <span class="blue bold">future</span> holds for you?</p>
+		<p>Get your personal <span class="yellow bold">horoscope</span> in a cookie!</p>
+	</div>
 
-<div class="cookies">
-	{{HTML::image("img/cookies.jpg") }}
-</div>
+	<div class="cookies">
+		{{HTML::image("img/cookies.jpg") }}
+	</div>
 
-<div class="givecookie">
-	<button class="button">Give me my cookie!</button>
+	<div class="givecookie">
+		<button class="button">Give me my cookie!</button>
+	</div>
 </div>
